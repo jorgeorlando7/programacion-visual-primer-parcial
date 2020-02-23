0b46f45bb65f5e8af1e574a7efdc3e4572857bcb
Jorge Orlando Hernandez de los Santos <53806823+jorgeorlando7@users.noreply.github.com>
2020-02-23 00:48:21 -0600

    Add files via upload

---
+++ Horse/src/horse/Horse.java
@@ -0,0 +1,27 @@
+public class Horse {
+    private String name;
+    private Integer birthyear;
+    private Color color;
+
+public void setName (String name){
+    this.name = name;
+}
+
+public void String getName(){
+    return this.name;
+}
+
+public void setbirthYear(Integer birthYear){
+    this.birthYear = birthYear;
+    public Integer getbirthYear;
+    return this.bithYear;
+}
+
+public void setColor(Color color){
+    this.color = color;
+}
+
+public Color getColor(){
+    return this.color;
+}
+}
