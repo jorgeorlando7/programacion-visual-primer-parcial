7fc344efcf4c6f7f5612c4796694f0bf01e02064
Jorge Orlando Hernandez de los Santos <53806823+jorgeorlando7@users.noreply.github.com>
2020-02-23 01:06:07 -0600

    Add files via upload

---
+++ state/TestState.java
@@ -0,0 +1,16 @@
+package State;
+
+public class TestState {
+
+	public static void main(String[] args) {
+		State state1 = new State("North Carolina", 10054192, "Raleigh", 431746, "Charlotte", 792862);
+		State state2 = new State("False Carolina", 4832000, "Columbia", 129272, "Columbia", 129272);
+		String stateString1 = state1.getStateData();
+		String stateString2 = state2.getStateData();
+		
+		System.out.println(stateString1);
+		System.out.println();
+		System.out.println(stateString2);
+	}
+
+}
